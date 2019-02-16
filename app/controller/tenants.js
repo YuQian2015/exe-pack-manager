11c1ae2661f7279da8b6f074b9791d90202797f5
Yu Qian <yuqian.124@gamil.com.com>
2019-02-16 22:20:35 +0800

    feature - find tenants api

--- app/controller/tenants.js
+++ app/controller/tenants.js
@@ -2,39 +2,55 @@ const Controller = require('egg').Controller;
 
 // 定义删除接口的请求参数规则
 const deleteRule = {
-  id: 'string',
+    id: 'string',
 };
 
 class TenantsController extends Controller {
 
-  async update() {
-    const ctx = this.ctx;
-    ctx.validate(deleteRule, ctx.params);
-    const result = await this.ctx.service.tenant.updateOneTenant({_id: ctx.params.id}, ctx.request.body);
-    if(result.ok) {
-        ctx.body = {
-            code: 200,
-            data: {},
-            success: true,
-            msg: `修改成功`
+    async index() {
+        const ctx = this.ctx;
+        try {
+            const tenants = await ctx.service.tenant.findTenant({});
+            ctx.body = {
+                code: 200,
+                data: tenants,
+                success: true,
+                msg: ``
+            };
+        } catch (err) {
+            throw err;
         }
     }
-  }
 
-
-  // 删除租户的方法
-  async destroy() {
-    const ctx = this.ctx;
-    ctx.validate(deleteRule, ctx.params);
-    const result = await this.ctx.service.tenant.deleteTenant(ctx.params.id);
-    if(result.ok) {
-        ctx.body = {
-            code: 200,
-            data: {},
-            success: true,
-            msg: `删除成功`
+    async update() {
+        const ctx = this.ctx;
+        ctx.validate(deleteRule, ctx.params);
+        const result = await this.ctx.service.tenant.updateOneTenant({_id: ctx.params.id}, ctx.request.body);
+        if (result.ok) {
+            ctx.body = {
+                code: 200,
+                data: {},
+                success: true,
+                msg: `修改成功`
+            }
         }
     }
-  }
+
+
+    // 删除租户的方法
+    async destroy() {
+        const ctx = this.ctx;
+        ctx.validate(deleteRule, ctx.params);
+        const result = await this.ctx.service.tenant.deleteTenant(ctx.params.id);
+        if (result.ok) {
+            ctx.body = {
+                code: 200,
+                data: {},
+                success: true,
+                msg: `删除成功`
+            }
+        }
+    }
 }
+
 module.exports = TenantsController;
